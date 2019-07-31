052cab72aa890dfa64c3dd6c6eb5b8c44734a758
Ling-Chun SO <lingchun.so@gmail.com>
2019-07-31 21:24:21 +0200

    remove limit base64 image storage

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -56,7 +56,6 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
