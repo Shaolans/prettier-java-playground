2a4d0dee3a2e5f5518be4c3b1d59d80363d2be3c
Ling-Chun SO <lingchun.so@gmail.com>
2019-09-17 13:45:54 +0200

    add sample code

--- src/components/CodeArea.vue
+++ src/components/CodeArea.vue
@@ -21,6 +21,26 @@ import 'brace/mode/less'
 import 'brace/theme/chrome'
 import 'brace/snippets/java'
 
+const codeSample = 
+`public class Prettier {
+  
+  public boolean prettierIsAwesome(int myFirstArgument, int mySecondArgument, int myThirdArgument, int myFourthArgument, int myFifthArgument) {
+    if(myFirstArgument == 1 && mySecondArgument == 2 && myThirdArgument == 3 && myFourthArgument == 4 && myFifthArgument == 5) {
+      return true;
+    }
+
+    // if do not want to format the next expression
+    // prettier-ignore
+    if(myFirstArgument == 1 && mySecondArgument == 1 && myThirdArgument == 1 && myFourthArgument == 1 && myFifthArgument == 1) {
+      return true;
+    }
+
+    MyObject mo = new MyObject();
+    return mo.very().very().very().very().very().very().very().very().very().very().very().very().very().longMethod();
+  }
+}
+`;
+
 export default {
   name: 'CodeArea',
   components: {
@@ -28,7 +48,7 @@ export default {
   },
   data () {
     return {
-      code: '',
+      code: codeSample,
       cst: {},
       optionCode: {
         fontSize: '16px',
